bd1a4fb25f1d68a28fc799475c503d3c65f626ab
Hyeonseok93 <houndscorporation@gmail.com>
2026-03-03 13:37:26 +0900

    second

---
+++ src/workshop/account/entity/Account.java
@@ -0,0 +1,43 @@
+package workshop.account.entity;
+
+public class Account {
+	private String custId;
+	private String acctId;
+	private int balance;
+	
+	//setter method (멤버변수 값 변경)
+	public void setCustId(String custId) {
+		this.custId = custId;
+	}
+	public void setAcctId(String acctId) {
+		this.acctId = acctId;
+	}
+//	public void setBalance(int balance) {
+//		this.balance = balance;
+//	}
+		
+	//getter method (멤버변수 값 조회)
+	public String getCustId() {
+		return custId;
+	}
+	public String getAcctId() {
+		return acctId;
+	}
+	public int getBalance() {
+		return balance;
+	}
+	
+	// 입금
+	public void deposit(int amount) {
+		// this.balance = this.balance + amount;
+		this.balance += balance;
+	}
+	
+	// 출금
+	public void withdraw(int amount) {
+		if(amount > balance) {
+			System.out.println("잔액 부족");
+		}
+		this.balance -= amount;
+	}
+}
